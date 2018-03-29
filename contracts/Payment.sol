319c63dcd461504f2bbb546d2cd968f86cf2ff93
Tasos Kalogeropoulos <tasoskalogero@gmail.com>
2018-03-29 18:52:22 +0200

    type fixed

--- contracts/Payment.sol
+++ contracts/Payment.sol
@@ -4,7 +4,7 @@ contract Payment {
 
     function payment(address dsAddr, address contAddr,bytes32 dsID, bytes32 containerID) public payable returns(uint){
         DatasetRepository dsRepo = DatasetRepository(dsAddr);
-        ContainerReposigory containerRepo = ContainerReposigory(contAddr);
+        ContainerRepository containerRepo = ContainerRepository(contAddr);
         var (dsCost, dsOwner) = dsRepo.getDatasetPurchaseInfoByID(dsID);
         var (contCost, contOwner) = containerRepo.getContainerPurchaseInfoByID(containerID); //TODO get from BDB
         contOwner.transfer(msg.value - dsCost);
@@ -17,6 +17,6 @@ contract DatasetRepository {
     function getDatasetPurchaseInfoByID(bytes32 _id) returns(uint, address);
 }
 
-contract ContainerReposigory{
+contract ContainerRepository{
     function getContainerPurchaseInfoByID(bytes32 _id)returns(uint, address);
 }
