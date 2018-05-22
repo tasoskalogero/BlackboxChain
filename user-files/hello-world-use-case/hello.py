29535e05c9c2fb60069fe6713f7eec0e454fbb9a
Tasos Kalogeropoulos <tasoskalogero@gmail.com>
2018-05-22 16:37:53 +0200

    user-files cleanup

--- user-files/hello-world-use-case/hello.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-import argparse
-
-parser = argparse.ArgumentParser(description='Download code from a url.')
-parser.add_argument("input", help="input data to print ", type=str)
-args = parser.parse_args()
-
-print("Hello ", args.input)
